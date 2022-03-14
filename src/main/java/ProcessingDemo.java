1a7cbfd4484f4d49f8ff7aa45870b41e50e1886f
swananana <swanasimran977@gmail.com>
2022-03-15 03:11:13 +0530

    Four balls moving at 4 different speeds.

--- src/main/java/ProcessingDemo.java
+++ src/main/java/ProcessingDemo.java
@@ -3,8 +3,11 @@ import processing.core.PApplet;
 public class ProcessingDemo extends PApplet{
     public static int height = 488;
     public static int width = 640;
-    public static int x=0;
-    public static int speed;
+    public static int x1=0;
+    public static int x2=0;
+    public static int x3=0;public static int x4=0;
+    public static int diameter = 20;
+
     public static void main(String... args){
         PApplet.main("ProcessingDemo");
     }
@@ -20,18 +23,39 @@ public class ProcessingDemo extends PApplet{
         loop();
     }
 
-    //@Override
+    @Override
     public void draw() {
-        ellipse(x,height/5,20,20);
-        ellipse(x,(2*height)/5,20,20);
-        ellipse(x,(3*height)/5,20,20);
-        ellipse(x,(4*height)/5,20,20);
-        x= x+1;
-        if(x>width){
-            x=0;
+        drawCircle();
+    }
+//  drawCircle draws the balls at specified heights and draws new balls at specified speed.
+    public void drawCircle(){
+//  Circle 1
+
+        ellipse(x1,(height)/5,diameter,diameter);
+        x1= x1+1;
+        if(x1>width){
+            x1=0;
+        }
+//  Circle 2
+
+        ellipse(x2,(2*height)/5,diameter,diameter);
+        x2= x2+2;
+        if(x2>width){
+            x2=0;
         }
 
+//  Circle 3
+        ellipse(x3,(3*height)/5,diameter,diameter);
+        x3= x3+3;
+        if(x3>width){
+            x3=0;
+        }
+
+//  Circle 4
+        ellipse(x4,(4*height)/5,diameter,diameter);
+        x4= x4+4;
+        if(x4>width){
+            x4=0;
+        }
     }
-
-
 }
